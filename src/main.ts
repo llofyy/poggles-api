799edbad2ff30ef85eff88e371a1ed5b9ac7311d
Lucas André <lucasandrefreitas1@gmail.com>
2023-03-29 21:58:34 -0300

    swagger configurated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { DocumentBuilder } from '@nestjs/swagger/dist';
+import { version } from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Poggles API')
+    .setDescription('An api to riot api')
+    .setVersion(version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
